3bdf300335717724448c4617888fa2675557e4f9
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-16 10:10:52 -0700

    Add map iteration through reservation data

---
+++ src/ResyContainer/ResyContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ResyContainer.css'
+import ResyCard from '../ResyCard/ResyCard';
+
+
+const ResyContainer = ({reservations}) => {
+  
+  const reservationCards = reservations.map(reservation => {
+    return (
+      <ResyCard
+        name={reservation.name}
+        date={reservation.date}
+        time={reservation.time}
+        number={reservation.number}
+        id={reservation.id}
+        key={reservation.id}
+      />
+    )
+  })
+
+  return (
+    <div className='resy-container'>
+      {reservationCards}
+    </div>
+  )
+}
+
+export default ResyContainer;
