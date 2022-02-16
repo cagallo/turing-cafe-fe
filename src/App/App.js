3723340cccdb9174df977242ddbaec20a9d78e61
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-16 09:39:30 -0700

    Create componentDidMount function

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import apiCalls from '../apiCalls';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations = [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    try {
+      let reservationData = await apiCalls.getReservationData();
+      this.setState({reservations: reservationData})
+    }
+    catch(error) {
+      this.setState({error: error.message})
+    }
+  }
+
   render() {
     return (
       <div className="App">
